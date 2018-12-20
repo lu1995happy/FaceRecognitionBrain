90a62d95637f7b7806b5fe59fc0d8a22bd1b6c27
Tianlin Lu <txl355@case.edu>
2018-12-19 16:17:32 -0800

    update database

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://secret-chamber-22611.herokuapp.com:3000/image', {
+        fetch('https://secret-chamber-22611.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
